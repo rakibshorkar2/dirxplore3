d16917ccd71cecd921758c8f7b9c3a3559002e7e
rakibshorkar2 <>
2026-06-25 19:02:26 +0600

    fix_launch_crash

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,10 +9,11 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+
     let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
     backgroundDownloader = BackgroundDownloader(messenger: controller.binaryMessenger)
 
-    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
